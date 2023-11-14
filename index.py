c70a9fdd745ca55ae469f4d8f8d8a80b6a566f70
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-14 09:12:36 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
 import mysql.connector
+
+# Configuración de la conexión a la base de datos
+conexion = mysql.connector.connect(
+    host="tu_host",
+    user="tu_usuario",
+    password="tu_contraseña",
+    database="tu_base_de_datos"
+)
